e10a8ce0d4a5eb6a316ac819d66300991023c2c4
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-28 17:43:16 -0300

    :sparkles: [feat] req-04

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,29 @@
+export default abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(n: string) {
+    this._name = n;
+    this._special = 0;
+    this._cost = 0;
+  }
+  
+  public get name() : string {
+    return this._name;
+  }
+
+  public get special() : number {
+    return this._special;
+  }
+
+  public get cost() : number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+   
+  abstract get energyType() : string;
+}
