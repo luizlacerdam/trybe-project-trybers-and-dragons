a3a3f77cd146c56a7b58912c5312648bf5f0ed63
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-29 10:02:15 -0300

    :sparkles: [feat] req-07

--- src/Character.ts
+++ src/Character.ts
@@ -56,7 +56,38 @@ export default class Character implements Fighter {
     return { type_: this._energy.type_, amount: this._energy.amount };
   }
 
-//   attack(enemy: Fighter): void {
-//     enemy.receiveDamage;
-//   }
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._defense; 
+    if (damage > 0) {
+      this._lifePoints = this.lifePoints - damage;
+    }
+    if (damage <= 0) {
+      this._lifePoints = this.lifePoints - 1;
+    }
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+
+  levelUp(): void {
+    this._maxLifePoints += getRandomInt(1, 10);
+    this._strength += getRandomInt(1, 10);
+    this._dexterity += getRandomInt(1, 10);
+    this._defense += getRandomInt(1, 10);
+    this._energy.amount = 10;
+    if (this._maxLifePoints > this._race.maxLifePoints) {
+      this._maxLifePoints = this._race.maxLifePoints;
+    }
+    this._lifePoints = this._maxLifePoints;
+  }
+
+  special?(enemy: Fighter): void {
+    const crit = (this._strength * 1.5) + (this._energy.amount * 1.2);
+    enemy.receiveDamage(crit);
+  }
 }
